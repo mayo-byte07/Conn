567d064b3aaf126e7cd787b41f88757dbdbafb46
PRINCE-ANTHONY <princeantony805@gmail.com>
2026-06-15 19:06:29 +0530

    feat: added scroll to top button

--- public/js/app.js
+++ public/js/app.js
@@ -409,3 +409,20 @@
     renderLinks();
   });
 })();
+// ─ ─ ─ Scroll to Top Button ─ ─ ─
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
